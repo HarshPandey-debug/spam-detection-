8cc7e52dc7beea19e6970a7850f935cb061c9a50
Harsh Pandey <harshp2034@gmail.com>
2026-04-15 20:32:31 +0530

    Delete trading_bot directory

--- trading_bot/bot/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Trading bot package for Binance Futures Testnet."""
-
-from .client import BinanceFuturesClient
-from .orders import OrderService
-
-__all__ = ["BinanceFuturesClient", "OrderService"]
